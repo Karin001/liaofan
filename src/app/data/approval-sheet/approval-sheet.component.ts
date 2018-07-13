c62e2d882ef52805f9151dd2872d49bf0590ad67
yuk <yukarinrin@163.com>
2018-07-13 19:24:41 +0800

    done

--- src/app/data/approval-sheet/approval-sheet.component.ts
+++ src/app/data/approval-sheet/approval-sheet.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataItemJson } from '../../DataTypeDefine/DataListJson';
 import { ItemTransmitService, TransType } from '../../service/item-transmit.service';
 import { instantiateDefaultStyleNormalizer } from '@angular/platform-browser/animations/src/providers';
@@ -8,8 +8,9 @@ import { instantiateDefaultStyleNormalizer } from '@angular/platform-browser/ani
   styleUrls: ['./approval-sheet.component.scss']
 })
 export class ApprovalSheetComponent implements OnInit {
-  @ViewChild('slider') slider;
   items: TransType;
+  gongSum;
+  guoSum;
   constructor(
     private itemTransmitServ: ItemTransmitService
   ) { }
@@ -18,26 +19,43 @@ export class ApprovalSheetComponent implements OnInit {
     this.itemTransmitServ.watchItems().subscribe(items => {
       if (items) {
         this.items = items;
+        this.gong_guo_sum();
       }
 
-    })
+    });
   }
   approval() {
     console.log(this.items);
   }
+  gong_guo_sum() {
+    if (this.items.gong && this.items.gong.length !== 0) {
+      this.gongSum = this.items.gong.map(v => v.customval).reduce((pre, cur) => {
+        return pre + cur;
+      });
+    }
+    if (this.items.guo && this.items.guo.length !== 0) {
+      this.guoSum = this.items.guo.map(v => v.customval).reduce((pre, cur) => {
+        return pre + cur;
+      });
+    }
+  }
   microConfigMinus(item) {
-   
-    item.customval = item.customval>item.val[0]?item.customval-1:item.val[0];
+
+    item.customval = item.customval > item.val[0] ? item.customval - 1 : item.val[0];
+    this.gong_guo_sum();
   }
   microConfigAdd(item) {
-  
-    item.customval = item.customval<item.val[item.val.length-1]?item.customval+1:item.val[item.val.length-1];
+
+    item.customval = item.customval < item.val[item.val.length - 1] ? item.customval + 1 : item.val[item.val.length - 1];
+    this.gong_guo_sum();
   }
-  pMulitiple(item){
-    item.customval+=item.val;
+  pMulitiple(item) {
+    item.customval += item.val;
+    this.gong_guo_sum();
   }
-  mMulitiple(item){
-    item.customval = item.customval>item.val?item.customval-item.val:item.val;
+  mMulitiple(item) {
+    item.customval = item.customval > item.val ? item.customval - item.val : item.val;
+    this.gong_guo_sum();
   }
 
 }
